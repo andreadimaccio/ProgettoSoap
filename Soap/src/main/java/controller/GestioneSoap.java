e9b63842a09e879a678b414fd51a0c64db7132ea
NeroKaios <matteo_samoila@yahoo.it>
2022-03-01 17:31:38 +0100

    MS

--- Soap/src/main/java/controller/GestioneSoap.java
+++ Soap/src/main/java/controller/GestioneSoap.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.sql.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.Postit;
+import model.Utenti;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -43,6 +45,26 @@ public class GestioneSoap extends HttpServlet {
 		Postit soap = new Postit();		
 	}
 	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String titolo = request.getParameter("titolo_postit");
+		String testo = request.getParameter("testo_postit");
+		Date dataInserimento = Date.valueOf(request.getParameter("data_inserimento_postit"));
+		Date dataPromemoria = Date.valueOf(request.getParameter("data_promemoria_postit"));
+		
+//		Date parsed = format.parse("20110210");
+//		java.sql.Date sql = new java.sql.Date(parsed.getTime());
+
+		if(!titolo.trim().equals("") && 
+				!testo.trim().equals("")  				
+				) {
+			Postit soap = new Postit();
+			soap.setTitoloPostit(titolo);
+			soap.setTestoPostit(testo);
+			soap.setDataInserimentoPostit(dataInserimento);
+			soap.setDataPromemoriaPostit(dataPromemoria);
+		}
+	}
+	
 	private Postit getSoapit(int id) {
 		em.getTransaction().begin();
 		Postit p = em.find(Postit.class, id);
@@ -53,8 +75,7 @@ public class GestioneSoap extends HttpServlet {
 	private void addSoapit(Postit soap) {
 		em.getTransaction().begin();
 		em.persist(soap);
-		em.getTransaction().commit();
-		System.out.println("Soap-it");
+		em.getTransaction().commit();		
 	}
 	
 	private void updateSoap(Postit soap) {
