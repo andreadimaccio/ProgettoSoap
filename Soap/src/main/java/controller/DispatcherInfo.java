46489f7ead35b81a242e4ef44d8412fda08e661c
andreadimaccio <100416782+andreadimaccio@users.noreply.github.com>
2022-03-03 09:59:18 +0100

    Update DispatcherInfo.java

--- Soap/src/main/java/controller/DispatcherInfo.java
+++ Soap/src/main/java/controller/DispatcherInfo.java
@@ -27,8 +27,7 @@ public class DispatcherInfo extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
 		if(request.getSession().getAttribute("utenteLogin") != null) {
 			Utenti u = (Utenti)request.getSession().getAttribute("utenteLogin");
-			System.out.println(u.getEmailUtente());
-			System.out.println(u.getPasswordUtente());
+			
 			if(u.getEmailUtente().equals("admin@admin.com") && u.getPasswordUtente().equals("admin")) {
 				request.getServletContext().getNamedDispatcher("GestioneAdmin").include(request, response);
 				request.getServletContext().getRequestDispatcher("/admin.jsp").forward(request, response);				
