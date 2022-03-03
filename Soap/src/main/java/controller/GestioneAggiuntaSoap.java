c500d0991fb0411f59c08a15c3fa298df892b5e1
NeroKaios <100416721+NeroKaios@users.noreply.github.com>
2022-03-03 14:48:27 +0100

    Update GestioneAggiuntaSoap.java

--- Soap/src/main/java/controller/GestioneAggiuntaSoap.java
+++ Soap/src/main/java/controller/GestioneAggiuntaSoap.java
@@ -17,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.Postit;
 import model.Utenti;
+import model.Categorie;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,7 +25,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name= "GestioneSoap" , urlPatterns = "/GestioneSoap")
+@WebServlet(name= "GestioneAggiuntaSoap" , urlPatterns = "/GestioneAggiuntaSoap")
 public class GestioneAggiuntaSoap extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	EntityManagerFactory emf;
@@ -37,53 +38,53 @@ public class GestioneAggiuntaSoap extends HttpServlet {
     @Override
 	public void init() throws ServletException {
 		super.init();
-		emf = Persistence.createEntityManagerFactory("WebProject_13_JPA_Login");
+		emf = Persistence.createEntityManagerFactory("Soap");
 		em = emf.createEntityManager();
 	}
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		Postit soap = new Postit();		
+			
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String titolo = request.getParameter("titolo_postit");
-		String testo = request.getParameter("testo_postit");		
-		LocalDate dataInserimento = LocalDate.now();
-		String anno = request.getParameter("anno");
-		String mese = request.getParameter("mese");
-		String giorno = request.getParameter("giorno");
-		long data = (long)Integer.parseInt(anno + mese + giorno);
-		Date dataPromemoria = new Date(data);
-	
+		String testo = request.getParameter("testo_postit");	
+		String data = request.getParameter("data");
+		int categoria = Integer.parseInt(request.getParameter("categoria_postit"));
+		Utenti utente = (Utenti)request.getServletContext().getAttribute("utenteLogin");
+		Date dataInserimento = Date.valueOf(LocalDate.now());
+		Date dataPromemoria = Date.valueOf(data);	
 		if(!titolo.trim().equals("") && 
 				!testo.trim().equals("")  				
 				) {
 			Postit soap = new Postit();
 			soap.setTitoloPostit(titolo);
 			soap.setTestoPostit(testo);
+			soap.setCategorie(getCategoria(categoria));
 			soap.setDataInserimento(dataInserimento);
 			soap.setDataPromemoria(dataPromemoria);
+			soap.setUtenti(utente);
 			addSoapit(soap);
 		}
-	}
-	
-	private Postit getSoapit(int id) {
-		em.getTransaction().begin();
-		Postit p = em.find(Postit.class, id);
-		em.getTransaction().commit();
-		return p;
-	}
-	
+	}	
 	private void addSoapit(Postit soap) {
 		em.getTransaction().begin();
 		em.persist(soap);
 		em.getTransaction().commit();		
 	}
-	
-	private void removeSoap(Postit soap) {
-		em.getTransaction().begin();
-		em.remove(soap);
-		em.getTransaction().commit();
+	private Categorie getCategoria(int id) {
+    	Categorie c = null;    	
+    	try {		    	
+		if(!em.getTransaction().isActive())
+		    em.getTransaction().begin();		
+		Query q = em.createQuery("SELECT c FROM Categorie c WHERE c.idCategoria = :param");
+		q.setParameter("param", id);		
+		c = (Categorie) q.getSingleResult();
+		em.getTransaction().commit();		
+    	}
+    	catch(Exception e){  		
+    	}
+    	return c;
 	}
 }
