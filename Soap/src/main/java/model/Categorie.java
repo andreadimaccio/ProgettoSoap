4b80782c6e1c61f2e7abc8ed8fd6d5f4827c17f3
NeroKaios <100416721+NeroKaios@users.noreply.github.com>
2022-03-03 18:06:08 +0100

    MS

--- Soap/src/main/java/model/Categorie.java
+++ Soap/src/main/java/model/Categorie.java
@@ -4,11 +4,6 @@ import java.io.Serializable;
 import javax.persistence.*;
 import java.util.List;
 
-
-/**
- * The persistent class for the categorie database table.
- * 
- */
 @Entity
 @NamedQuery(name="Categorie.findAll", query="SELECT c FROM Categorie c")
 public class Categorie implements Serializable {
