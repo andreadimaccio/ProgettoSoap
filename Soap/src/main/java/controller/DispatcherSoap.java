b2f7ee292c966b105429115c595f62f97664b7b6
NeroKaios <100416721+NeroKaios@users.noreply.github.com>
2022-03-03 14:47:50 +0100

    Add files via upload

---
+++ Soap/src/main/java/controller/DispatcherSoap.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dispatchersoap")
+public class DispatcherSoap extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+   
+    public DispatcherSoap() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		if(request.getParameter("action") != null && request.getParameter("action").equals("aggiungisoap")) {
+			request.getServletContext().getRequestDispatcher("/creasoap.jsp").forward(request, response);
+			request.getServletContext().getNamedDispatcher("GestioneAggiuntaSoap").include(request, response);
+			request.getServletContext().getRequestDispatcher("/rubrica.jsp").forward(request, response);
+		}
+		else {
+			request.getServletContext().getRequestDispatcher("dispatchergenerale").forward(request, response);
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
