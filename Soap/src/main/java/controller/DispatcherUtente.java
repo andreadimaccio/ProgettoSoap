18891e98af7d4c48a6bdf39d07cfdea625e27865
NeroKaios <100416721+NeroKaios@users.noreply.github.com>
2022-03-04 09:40:15 +0100

    MS

---
+++ Soap/src/main/java/controller/DispatcherUtente.java
@@ -0,0 +1,36 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/dispatcherutente")
+public class DispatcherUtente extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public DispatcherUtente() {
+        super();
+
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		if(action != null && action.equals("logout")) {
+			request.getSession().setAttribute("utentelogin", null);
+			response.sendRedirect("dispatchergenerale");
+		} 
+		else if(action != null && action.equals("edit")) {
+			request.getServletContext().getRequestDispatcher("/modificaprofilo.jsp").forward(request, response);
+		}
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getServletContext().getNamedDispatcher("GestioneEditUtente").include(request, response);
+		request.getServletContext().getRequestDispatcher("/homepageutente.jsp").forward(request, response);
+	}
+
+}
