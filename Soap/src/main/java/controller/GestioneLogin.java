0267e2cdec1ac0990eb18b50322fda3404c9a569
NeroKaios <100416721+NeroKaios@users.noreply.github.com>
2022-03-04 12:50:51 +0100

    MS

--- Soap/src/main/java/controller/GestioneLogin.java
+++ Soap/src/main/java/controller/GestioneLogin.java
@@ -19,47 +19,51 @@ import model.Utenti;
 public class GestioneLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	EntityManagerFactory emf;
-    EntityManager em;
-    
-    public Utenti getUtenteLogin(String email, String password) {
-    	Utenti u = null;    	
-    	try {		    	
-		if(!em.getTransaction().isActive())
-		    em.getTransaction().begin();		
-		Query q = em.createQuery("SELECT u FROM Utenti u WHERE u.emailUtente = :param AND u.passwordUtente = :param1");
-		q.setParameter("param", email);
-		q.setParameter("param1", password);
-		u = (Utenti) q.getSingleResult();
-		em.getTransaction().commit();		
-    	}
-    	catch(Exception e){  		
-    	}
-    	return u;
+	EntityManager em;
+
+	public Utenti getUtenteLogin(String email, String password) {
+		Utenti u = null;    	
+		try {		    	
+			if(!em.getTransaction().isActive())
+				em.getTransaction().begin();		
+			Query q = em.createQuery("SELECT u FROM Utenti u WHERE u.emailUtente = :param AND u.passwordUtente = :param1");
+			q.setParameter("param", email);
+			q.setParameter("param1", password);
+			u = (Utenti) q.getSingleResult();
+			em.getTransaction().commit();		
+		}
+		catch(Exception e){  		
+		}
+		return u;
 	}
-    
-    public GestioneLogin() {   	
-        super();  
-    }
-    
-    @Override
+
+	public GestioneLogin() {   	
+		super();  
+	}
+
+	@Override
 	public void init() throws ServletException {
 		super.init();
-		  emf = Persistence.createEntityManagerFactory("Soap");
-	      em = emf.createEntityManager();
+		emf = Persistence.createEntityManagerFactory("Soap");
+		em = emf.createEntityManager();
 	}
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
-		
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String email = request.getParameter("email_utente");
 		String password = request.getParameter("password_utente");
-		if(!email.trim().equals("") && !password.trim().equals("")) {
-			Utenti u = getUtenteLogin(email, password);
-			HttpSession session = request.getSession();
-			session.setAttribute("utenteLogin", u);		
+		if(request.getSession().getAttribute("utenteLogin") == null){
+			if(!email.trim().equals("") && !password.trim().equals("")) {
+				Utenti u = getUtenteLogin(email, password);
+				HttpSession session = request.getSession();
+				session.setAttribute("utenteLogin", u);	
+			}						
+		}
+		else {			
 		}
 	}
 
