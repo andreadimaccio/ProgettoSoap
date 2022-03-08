1b8e30e951d34bcc623dad881dd741cb6607cd35
LeonardoFacchino <75796996+LeonardoFacchino@users.noreply.github.com>
2022-03-08 14:54:48 +0100

    Update toTop.js

--- Soap/src/main/webapp/toTop.js
+++ Soap/src/main/webapp/toTop.js
@@ -1,21 +1,9 @@
 let mybutton = document.getElementById("btn-back-to-top");
 
-		window.onscroll = function() {
-			scrollFunction();
-		};
+mybutton.addEventListener("click", backToTop);
 
-		function scrollFunction() {
-			if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-				mybutton.style.display = "block";
-			} else {
-				mybutton.style.display = "none";
-			}
-		}
-
-		mybutton.addEventListener("click", backToTop);
-
-		function backToTop() {
-			document.body.scrollTop = 0;
-			document.documentElement.scrollTop = 0;
-		}
+function backToTop() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
 		
